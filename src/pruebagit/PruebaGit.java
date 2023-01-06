d3fe289a51e073feaf4ebffe599e7621fe5c7db2
ChilyGarcia <jugada160324@gmail.com>
2023-01-06 17:31:21 -0500

    mi primer commit

---
+++ src/pruebagit/PruebaGit.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package pruebagit;
+
+import Vista.Vista;
+
+/**
+ *
+ * @author jugad
+ */
+public class PruebaGit {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) 
+    {
+        Vista.main(args);
+    }
+    
+}
